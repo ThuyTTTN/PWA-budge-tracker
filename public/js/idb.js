95e608cb75f7db08b1d63cf62377e1d4217e6bd3
Thuy Nguyen <ttruong06@gmail.com>
2022-06-25 12:09:24 -0400

    save data on upload

--- public/js/idb.js
+++ public/js/idb.js
@@ -38,9 +38,54 @@ function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_budget'], 'readwrite');
   
-    // access the object store for `new_pizza`
+    // access the object store for `new_budget`
     const budgetObjectStore = transaction.objectStore('new_budget');
   
     // add record to your store with add method
     budgetObjectStore.add(record);
   }
+
+
+//collect all data from new_budget object store in IndexedDB and POST it to ther server
+function uploadBudget() {
+    //open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    //get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    //upon a successful .getAll(), run this function
+    getAll.onsuccess = function () {
+        //if there was data in indexedDB's store, lets send it to the api server
+        if (getAll.result.length > 0 ) {
+            fetch('/api', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                //open one more transaction
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                //access the new_budget object store
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                //clear all items in your store
+                budgetObjectStore.clear();
+
+                alert ('All saved budget has been submitted');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+};

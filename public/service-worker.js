603a90965e505f43bc935c73197654ff7c0f5e81
Thuy Nguyen <ttruong06@gmail.com>
2022-06-26 17:07:04 -0400

    finished service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,26 @@ const FILES_TO_CACHE = [
     "/css/styles.css"
 ];
 
+//tells service worker how to retrieve info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -18,3 +38,24 @@ self.addEventListener('install', function (e) {
     )
 
 })
+
+//activation - clear out any old data from the cache, then tell service worker how to manage caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+ });
+          cacheKeeplist.push(CACHE_NAME);
+
+          return Promise.all(
+            keyList.map(function (key, i) {
+              if (cacheKeeplist.indexOf(key) === -1) {
+                console.log("deleting cache : " + keyList[i]);
+                return caches.delete(keyList[i]);
+              }
+            })
+          );
+        })
+      );
+    });
